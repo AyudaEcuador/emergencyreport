ce020e2e55b9142e4209e23673b251cac651ca7b
wikicarlos <wikicarlos@gmail.com>
2016-04-25 18:12:15 -0500

    Up to colaborate.

--- genericform/admin.py
+++ genericform/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from genericform.models import GenericForm, GenericField
 # Register your models here.
+
+class GenericFormAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(GenericForm,GenericFormAdmin)
+
+class GenericFieldAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(GenericField,GenericFieldAdmin)
--- genericform/models.py
+++ genericform/models.py
@@ -14,3 +14,53 @@ class GenericForm(models.Model):
     class Meta:
         pass
 
+
+class GenericField(models.Model):
+    TIPO_CHOICES = (
+        (1, 'NUMERICO'),
+        (2, 'TEXTO'),
+        (3, 'SELECCIONABLE_TEXTO'),
+    )
+
+    genericform = models.ForeignKey(GenericForm)
+    nombre =  models.CharField(max_length=20)
+    ordinal = models.IntegerField()
+    tipo = models.IntegerField(choices=TIPO_CHOICES)
+    validation_dict = models.TextField()
+
+    def __unicode__(self):
+        return str(self.nombre)
+
+    class Meta:
+        pass
+
+class GenericParentOption(models.Model):
+    TIPO_CHOICES = (
+        (1, 'NUMERICO'),
+        (2, 'TEXTO'),
+        (3, 'VERDADERO/FALSO')
+    )
+    genericfield = models.ForeignKey(GenericField)
+
+    def __unicode__(self):
+        return str(self.id)
+
+    class Meta:
+        pass
+
+
+class GenericOption(models.Model):
+    TIPO_CHOICES = (
+        (1, 'NUMERICO'),
+        (2, 'TEXTO'),
+        (3, 'VERDADERO/FALSO')
+    )
+    genericfield = models.ForeignKey(GenericField)
+    nombre = models.CharField(max_length=15)
+    valor = models.CharField(max_length=20,help_text='El valor del campo sera convertido')
+
+    def __unicode__(self):
+        return str(self.id)
+
+    class Meta:
+        pass
