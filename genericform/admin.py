c12b7599248fe1737279d798269d572d3e330891
andres <andres_anies@hotmail.com>
2016-04-28 17:25:42 -0500

    ingreso de formularios genericos con campos anidados

--- genericform/admin.py
+++ genericform/admin.py
@@ -1,11 +1,29 @@
 from django.contrib import admin
 from genericform.models import GenericForm, GenericField, GenericParentOption, GenericOption
+from nested_inline.admin import NestedStackedInline, NestedModelAdmin
 
 
 # Register your models here.
 
-class GenericFormAdmin(admin.ModelAdmin):
-    pass
+class GenericOptionInline(NestedStackedInline):
+    model = GenericOption
+    extra = 1
+
+
+class GenericParentOptionInline(NestedStackedInline):
+    model = GenericParentOption
+    extra = 0
+    inlines = [GenericOptionInline]
+
+
+class GenericFieldInline(NestedStackedInline):
+    model = GenericField
+    extra = 1
+    inlines = [GenericParentOptionInline]
+
+
+class GenericFormAdmin(NestedModelAdmin):
+    inlines = [GenericFieldInline]
 
 
 admin.site.register(GenericForm, GenericFormAdmin)
