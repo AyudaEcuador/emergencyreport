db9cbcc0e35583ad5a65428016f31cde9f6db4f1
andres <andres_anies@hotmail.com>
2016-05-04 21:43:41 -0500

    campo tipo punto geografico

--- accounts/models.py
+++ accounts/models.py
@@ -4,14 +4,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Institucion(models.Model):
+class Lugar(models.Model):
+    LUGAR_CHOICES = (
+        (1, 'Refugio'),
+        (2, 'albergue'),
+        (3, 'campamento'),
+    )
+
     nombre = models.CharField(max_length=64)
+    tipo = models.IntegerField(choices=LUGAR_CHOICES)
 
 
 class Perfil(models.Model):
     usuario = models.ForeignKey(User)
-    institucion = models.ForeignKey(Institucion)
+    lugar = models.ForeignKey(Lugar)
     telefono = models.CharField(max_length=15)
-
-
-
